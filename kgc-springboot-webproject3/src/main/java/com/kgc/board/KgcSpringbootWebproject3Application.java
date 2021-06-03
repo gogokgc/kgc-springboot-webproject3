c656a1c16f8dace7b714c741be3ff0b97b349a46
gogokgc <gogokgc@gmail.com>
2021-06-03 19:31:08 +0900

    test

---
+++ kgc-springboot-webproject3/src/main/java/com/kgc/board/KgcSpringbootWebproject3Application.java
@@ -0,0 +1,13 @@
+package com.kgc.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcSpringbootWebproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcSpringbootWebproject3Application.class, args);
+	}
+
+}
