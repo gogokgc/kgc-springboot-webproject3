2cf9627149bfcf87e9031661368ab5711b806e34
gogokgc <gogokgc@gmail.com>
2021-06-03 20:58:29 +0900

    ignore test

--- kgc-springboot-webproject3/src/test/java/com/kgc/board/KgcSpringbootWebproject3ApplicationTests.java
+++ kgc-springboot-webproject3/src/test/java/com/kgc/board/KgcSpringbootWebproject3ApplicationTests.java
@@ -3,11 +3,11 @@ package com.kgc.board;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class KgcSpringbootWebproject3ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class KgcSpringbootWebproject3ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
