8e653ae6cdb9f394f9ac551068c01b3eeb798232
gogokgc <79429581+gogokgc@users.noreply.github.com>
2021-06-03 21:21:16 +0900

    Delete kgc-springboot-webproject3 directory

--- kgc-springboot-webproject3/src/main/java/com/kgc/board/domain/posts/PsPostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kgc.board.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PsPostsRepository extends JpaRepository<PsPosts, Long>{
-
-//	@Query("SELECT p FROM Posts p ORDER BY p.id DESC")
-//	List<PsPosts> findAllDesc();
-}
