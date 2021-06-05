2595cfbfb35c88d33c07117bcf39b16ac3053a9c
gogokgc <gogokgc@gmail.com>
2021-06-05 18:18:40 +0900

    fix deploy.sh 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,4 +35,4 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.properties \
- $REPOSITORY/$JAR_NAME 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
