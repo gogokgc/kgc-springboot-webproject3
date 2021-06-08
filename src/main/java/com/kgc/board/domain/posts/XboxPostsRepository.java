c20141a9a9c3a6599deb5bbcbfdbd722941ff1f1
gogokgc <gogokgc@gmail.com>
2021-06-08 19:42:56 +0900

    revert

--- src/main/java/com/kgc/board/domain/posts/XboxPostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kgc.board.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface XboxPostsRepository extends JpaRepository<XboxPosts, Long>{
-
-//	@Query("SELECT p FROM Posts p ORDER BY p.id DESC")
-//	List<PsPosts> findAllDesc();
-}
