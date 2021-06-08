f7bd198dab09559346ea698edfb972560155d00a
gogokgc <gogokgc@gmail.com>
2021-06-08 19:14:34 +0900

    prepare xbox post

--- src/main/java/com/kgc/board/domain/posts/XboxPostsRepository.java
+++ src/main/java/com/kgc/board/domain/posts/XboxPostsRepository.java
@@ -2,7 +2,7 @@ package com.kgc.board.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface XboxPostsRepository extends JpaRepository<PsPosts, Long>{
+public interface XboxPostsRepository extends JpaRepository<XboxPosts, Long>{
 
 //	@Query("SELECT p FROM Posts p ORDER BY p.id DESC")
 //	List<PsPosts> findAllDesc();
