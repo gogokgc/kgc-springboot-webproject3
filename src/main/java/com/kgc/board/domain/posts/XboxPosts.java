725c6b1d609b9b4299a02111d2832f78f5ccd45a
gogokgc <gogokgc@gmail.com>
2021-06-08 18:43:13 +0900

    prepare games posts

---
+++ src/main/java/com/kgc/board/domain/posts/XboxPosts.java
@@ -0,0 +1,47 @@
+package com.kgc.board.domain.posts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import com.kgc.board.domain.BaseTimeEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class XboxPosts extends BaseTimeEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 500, nullable = false)
+	private String title;
+	
+	@Lob
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+	
+	private String author;
+
+	@Builder
+	public XboxPosts(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+	
+	public void update(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+	
+	
+}
